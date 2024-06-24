745944d5a5630e0fc65098f3da22e947103e4fd8
Ekaterina <kate.pzh@gmail.com>
2024-06-24 22:55:50 +0300

    ApiCoreRequests

---
+++ src/test/java/tests/UserRegisterTest.java
@@ -0,0 +1,63 @@
+package tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+import lib.Assertions;
+import lib.BaseTestCase;
+import lib.DataGenerator;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UserRegisterTest extends BaseTestCase {
+    @Test
+    public void testCreateUserWithExistingEmail() {
+        String email = "vinkotov@example.com";
+
+        Map<String, String> userData = new HashMap<>();
+        userData.put("email", email);
+        userData = DataGenerator.getRegistrationData(userData);
+//        userData.put("password", "123");
+//        userData.put("username", "learnqa");
+//        userData.put("firstName", "learnqa");
+//        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        System.out.println(responseCreateAuth.asString());
+        System.out.println(responseCreateAuth.statusCode());
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 400);
+        Assertions.assertResponseTextEquals(responseCreateAuth, "Users with email '" + email + "' already exists");
+    }
+
+    @Test
+    public void testCreateUserSuccessfully() {
+        String email = DataGenerator.getRandomEmail();
+
+        Map<String, String> userData = DataGenerator.getRegistrationData();
+//        Map<String, String> userData = new HashMap<>();
+//        userData.put("email", email);
+//        userData.put("password", "123");
+//        userData.put("username", "learnqa");
+//        userData.put("firstName", "learnqa");
+//        userData.put("lastName", "learnqa");
+
+        Response responseCreateAuth = RestAssured
+                .given()
+                .body(userData)
+                .post("https://playground.learnqa.ru/api/user/")
+                .andReturn();
+
+        System.out.println(responseCreateAuth.asString());
+        System.out.println(responseCreateAuth.statusCode());
+
+        Assertions.assertResponseCodeEquals(responseCreateAuth, 200);
+        Assertions.assertJsonHasField(responseCreateAuth, "id");
+    }
+}
